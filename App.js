43fe104d3dc7e46ceb8a69187e886c8c5fdc2feb
raechelleannecajes/raechelleannecajes-rgb <raechelleannecajes@gmail.com>
2025-09-20 23:53:10 +0800

    Color Changer App

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
 
 export default function App() {
-  return <CounterApp />;
+  return <ColorChangerApp />;
 }
