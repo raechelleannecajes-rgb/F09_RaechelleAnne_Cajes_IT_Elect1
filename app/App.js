90d9ebb7f8dde27dbe08dd0d2b0126475672be12
raechelleannecajes <raechelleannecajes@gmail.com>
2025-10-26 15:58:45 +0800

    Update the code

--- app/App.js
+++ app/App.js
@@ -1,11 +1,5 @@
-import { SafeAreaView, StatusBar } from "react-native";
 import MessengerAndCommentApp from "./screens/MessengerAndCommentApp";
 
 export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <MessengerAndCommentApp />
-    </SafeAreaView>
-  );
+  return <MessengerAndCommentApp />;
 }
